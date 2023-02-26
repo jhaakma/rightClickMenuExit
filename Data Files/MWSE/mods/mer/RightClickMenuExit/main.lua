9116dfdbf4e68eb70e9bfa90134363ec30a1f518
jhaakma <jchaakma@gmail.com>
2023-02-26 23:04:35 +1300

    Fix menu closing order

--- Data Files/MWSE/mods/mer/RightClickMenuExit/main.lua
+++ Data Files/MWSE/mods/mer/RightClickMenuExit/main.lua
@@ -1,55 +1,43 @@
 local config = require("mer.RightClickMenuExit.config")
---in order of priority
 
-local function resolveId(id)
-    if type(id) == "string" then
-        return tes3ui.registerID(id)
-    else
-        return id
-    end
-end
-
---Allow exiting companion share menu like other menus
 local function closeMenu()
-    local topMenu = tes3ui.getMenuOnTop()
-    if not topMenu then
+    local menuOnTop = tes3ui.getMenuOnTop()
+    if not menuOnTop then
         return
     end
-    --first check that at least one of our menus is on top
-    --But it may not be the one we "close", i.e inventory menu might
-    local menuOnTop
-    for _, data in ipairs(config.buttonMapping) do
-        if resolveId(topMenu.id) == resolveId(data.menu) then
-            menuOnTop = true
-            break
-        end
-    end
-    if menuOnTop then
-        for _, data in ipairs(config.buttonMapping) do
-            local menu = tes3ui.findMenu(data.menu)
-            if menu and resolveId(menu.id) == resolveId(data.menu) then
-                local closeButton = menu:findChild(data.button)
-                if closeButton and closeButton.visible then
+    local rootUI = menuOnTop.parent
+    -- iterate over rootUI.children in reverse order
+    for i = #rootUI.children, 1, -1 do
+        --find registered menu with close button
+        local menu = rootUI.children[i]
+        if menu.visible then
+            local menuData = config[menu.name]
+            if menuData then
+                local button = menu:findChild(menuData.closeButton)
+                if button and button.visible then
                     tes3.worldController.menuClickSound:play()
-                    closeButton:triggerEvent("mouseClick")
+                    button:triggerEvent("mouseClick")
+                    return
                 end
-                return
             end
         end
     end
 end
 
 local function onMouseButtonDown(e)
-    if e.button == tes3.worldController.inputController.inputMaps[19].code then
-        closeMenu()
+    if tes3ui.menuMode() then
+        if e.button == tes3.worldController.inputController.inputMaps[19].code then
+            closeMenu()
+        end
     end
 end
+event.register("mouseButtonDown", onMouseButtonDown)
 
 local function onKeyDown(e)
-    if e.keyCode ~= 1 and e.keyCode == tes3.worldController.inputController.inputMaps[19].code then
-        closeMenu()
+    if tes3ui.menuMode() then
+        if e.keyCode ~= 1 and e.keyCode == tes3.worldController.inputController.inputMaps[19].code then
+            closeMenu()
+        end
     end
 end
-
 event.register("keyDown", onKeyDown)
-event.register("mouseButtonDown", onMouseButtonDown)
